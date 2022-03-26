0110ddaa09b66749c99483fe2a97857ea26b5a52
unknown <brunor.gambim@gmail.com>
2022-03-26 16:27:37 -0300

    design da interface alterado

--- ArenaCampestre/src/main/java/com/grupo4/ArenaCampestre/controller/UserController.java
+++ ArenaCampestre/src/main/java/com/grupo4/ArenaCampestre/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.grupo4.ArenaCampestre.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
